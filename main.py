95dc408077d9fb7f807e33e1dc6ce0213990a51a
Ігор Квасниця <ih.kvasnytsia@gmail.com>
2026-09-08 20:41:14 +0300

    mvc

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import tkinter as tk
-from gui.app import App
+from controllers.app_controller import AppController
+from models.ascii_data import ASCIIData
+from services.character_search_service import CharacterSearchService
+from services.localization_service import LocalizationService
+from views.main_view import MainView
+
 
 def main():
-   App().run()
+
+    data = ASCIIData()
+    search_engine = CharacterSearchService(data)
+    localization_service = LocalizationService()
+    app_controller = AppController(search_engine, data, localization_service)
+    
+    MainView(app_controller).run()   
 
 
 if __name__ == "__main__":
